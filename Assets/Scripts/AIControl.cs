7bae2f7046cc304b5e6b23343a11b99752e887fb
Bogdan <bogdancenusa19@gmail.com>
2024-10-10 20:48:29 +0300

    Optimized AIControl and added Underworld map

--- Assets/Scripts/AIControl.cs
+++ Assets/Scripts/AIControl.cs
@@ -1,53 +1,72 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class AIControl : MonoBehaviour
 {
-    public GameObject character;  // Referința la caracterul AI
-    public GameObject player;     // Referința la player
-    public float moveSpeed = 2.0f;  // Viteza de deplasare a caracterului AI
-    public float minDistance = 1.5f; // Distanța minimă între AI și player
-    public float maxDistance = 3.0f; // Distanța maximă între AI și player
-    private Vector3 targetPosition;  // Poziția țintă a caracterului
+    [SerializeField] private GameObject spirit;
+    [SerializeField] private Animator animator;
+    [SerializeField] private GameObject player;
+    [SerializeField] private float moveSpeed = 2.0f;  
+    [SerializeField] private float minDistance = 1.5f; 
+    [SerializeField] private float maxDistance = 3.0f; 
+    private Vector3 targetPosition; 
+
+    private bool isUnderControl = false;
     
     void Start()
     {
         StartCoroutine(CheckPlayerPosition());
     }
 
+    private void Update()
+    {
+        if (!isUnderControl)
+        {
+            KeepFacingUpdated();
+        }
+    }
+
+    private void KeepFacingUpdated()
+    {
+        if (spirit.transform.position.x > player.transform.position.x)
+        {
+            animator.transform.localScale = new Vector3(1.0f, 1.0f, -1);
+        }
+        else
+        {
+            animator.transform.localScale = new Vector3(1.0f, 1.0f, 1);
+        }
+    }
+
     IEnumerator CheckPlayerPosition()
     {
         while (true)
         {
-            // Calculăm distanța actuală între AI și player
-            float distanceToPlayer = Vector3.Distance(character.transform.position, player.transform.position);
-
-            // Calculăm o distanță random de păstrat între minDistance și maxDistance
+            float distanceToPlayer = Vector3.Distance(spirit.transform.position, player.transform.position);
+            
             float desiredDistance = Random.Range(minDistance, maxDistance);
-
-            // Dacă AI-ul este prea aproape de player sau prea departe, se va apropia/lărgi distanța
+            
             if (distanceToPlayer > desiredDistance)
             {
-                // Calculăm direcția către player
-                Vector3 direction = (player.transform.position - character.transform.position).normalized;
-
-                // Setăm ținta la distanța dorită de player
+                Vector3 direction = (player.transform.position - spirit.transform.position).normalized;
+                
                 targetPosition = player.transform.position - direction * desiredDistance;
 
-                // Mișcăm caracterul în direcția țintei, cu o viteză mai lentă
-                character.transform.position = Vector3.MoveTowards(character.transform.position, targetPosition, moveSpeed * Time.deltaTime);
+                if (!isUnderControl)
+                {
+                    spirit.transform.position = Vector3.MoveTowards(spirit.transform.position, targetPosition, moveSpeed * Time.deltaTime);
+                }
             }
-
-            // Așteptăm un frame înainte de a face următoarea verificare
             yield return null;
         }
     }
 
-    public void SwapCharacters()
-    {   
-        GameObject temp = player; 
-        player = character; 
-        character = temp;
+    public void SetIsUnderControl(bool state)
+    {
+        isUnderControl = state;
     }
+    
 }
--- Assets/Scripts/SwitchControl.cs
+++ Assets/Scripts/SwitchControl.cs
@@ -4,12 +4,19 @@ using System.Collections.Generic;
 using Cainos.CustomizablePixelCharacter;
 using Cainos.PixelArtMonster_Dungeon;
 using Cinemachine;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class SwitchControl : MonoBehaviour
 {
     [Header("AI")] 
     [SerializeField] private AIControl _aiControl;
+
+    [Header("Worlds")] 
+    [SerializeField] private GameObject realWorld;
+
+    [SerializeField] private GameObject underworld;
     
     [Space][Header("Characters")]
     [SerializeField] private GameObject player;
@@ -30,9 +37,15 @@ public class SwitchControl : MonoBehaviour
     [SerializeField] private MonsterFlyingController _controllerSpirit;
     [SerializeField] private MonsterInputMouseAndKeyboard _inputMouseAndKeyboardSpirit;
     
+    
 
     private bool isControlAtTomer = true;
-    
+
+    private void Start()
+    {
+        _pixelMonster.Alpha = 0.5f;
+    }
+
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.E))
@@ -45,40 +58,48 @@ public class SwitchControl : MonoBehaviour
     {
         if (isControlAtTomer)
         {
+            underworld.SetActive(true);
+            realWorld.SetActive(false);
             ResetMovement(_controller);
             
+            _aiControl.SetIsUnderControl(true);
             _pixelMonster.enabled = true;
             _controllerSpirit.enabled = true;
             _inputMouseAndKeyboardSpirit.enabled = true;
-
+            
             _pixelCharacter.enabled = false;
             _controller.enabled = false;
             _inputMouseAndKeyboard.enabled = false;
 
             camera.Follow = spirit.transform;
 
+            _pixelMonster.Alpha = 1f;
+            _pixelCharacter.Alpha = 0.5f;
             isControlAtTomer = false;
             
         }
         else
         {
+            realWorld.SetActive(true);
+            underworld.SetActive(false);
             ResetMovement(_controllerSpirit);
             
             _pixelCharacter.enabled = true;
             _controller.enabled = true;
             _inputMouseAndKeyboard.enabled = true;
             
+            _aiControl.SetIsUnderControl(false);
             _pixelMonster.enabled = false;
             _controllerSpirit.enabled = false;
             _inputMouseAndKeyboardSpirit.enabled = false;
 
             camera.Follow = player.transform;
 
+            _pixelCharacter.Alpha = 1f;
+            _pixelMonster.Alpha = 0.5f;
             isControlAtTomer = true;
             
         }
-
-        _aiControl.SwapCharacters();
     }
     
     private void ResetMovement(PixelCharacterController controller)
